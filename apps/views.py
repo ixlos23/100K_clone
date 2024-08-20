fbd46c58011545437f7573fd476f478b6bd4a680
ixlos23 <izzatmatkarimov090@gmail.com>
2024-08-20 13:05:47 +0500

    Update views.py
    g

--- apps/views.py
+++ apps/views.py
@@ -5,11 +5,3 @@ class HomeView(TemplateView):
     template_name = 'apps/home.html'
 
 
-"""
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/ixlos23/100K_clone.git
-git push -u origin main
-"""
