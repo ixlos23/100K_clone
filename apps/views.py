f2ad285553ac3f09b06e416a60d783ccaf94373e
ixlos23 <izzatmatkarimov090@gmail.com>
2024-08-20 12:52:40 +0500

    first commit

---
+++ apps/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'apps/home.html'
+
+
+"""
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ixlos23/100K_clone.git
+git push -u origin main
+"""
